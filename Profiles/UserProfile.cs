c7a0a4045f05f064e2bc5827dfc29877fbd6e0c4
Melory Ayala <melory.ayala@gmail.com>
2023-11-15 13:46:23 +0100

    chore: add user autoMapper profile

---
+++ Profiles/UserProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using UserApi.Data.Dtos;
+using UserApi.Models;
+
+namespace UserApi.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<CreateUserDto, User>();
+    }
+}
