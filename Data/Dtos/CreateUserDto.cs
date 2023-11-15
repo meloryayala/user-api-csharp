0ebcc41f78ac4e8f932eac5e20bd53891fdf1ef2
Melory Ayala <melory.ayala@gmail.com>
2023-11-15 13:40:38 +0100

    feat: add createUserDto

---
+++ Data/Dtos/CreateUserDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace UserApi.Data.Dtos;
+
+public class CreateUserDto
+{
+    [Required]
+    public string Username { get; set; }
+    
+    [Required] 
+    public DateType BornDate { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    [Required]
+    [Compare("Password")]
+    public string RePassword { get; set; }
+}
