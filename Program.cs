e4cb0f353bd54a37fb761744482a90233482ca3c
Melory Ayala <melory.ayala@gmail.com>
2023-11-15 14:31:16 +0100

    chore: add configuration of DbContext and Identity

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserApi.Data;
+using UserApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("UserConnection");
+
+builder.Services.AddDbContext<UserDbContext>(
+    opts =>
+    {
+        opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    });
+
+builder.Services
+    .AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<UserDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
